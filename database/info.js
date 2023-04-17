bfedd413415f99f5bcec1ce208d7c06a531cd06c
ahmedtarek52 <117595019+ahmedtarek52@users.noreply.github.com>
2023-04-17 21:09:22 +0200

    Add files via upload

---
+++ database/info.js
@@ -0,0 +1,31 @@
+export const info = [
+    {
+        id: 1,
+        name: "ahmed",
+    },
+    {
+        id: 2,
+        name: "Tarek",
+    },
+    {
+        id: 3,
+        name: "ali",
+    },
+    {
+        id: 4,
+        name: "waled",
+    },
+    {
+        id: 5,
+        name: "gamal",
+    },
+    {
+        id: 6,
+        name: "adham",
+    },
+    {
+        id: 7,
+        name: "nour",
+    },
+];
+
